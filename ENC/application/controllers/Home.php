e383760eb319168b05178674f235d8187de5f185
yangy <yangyong@starnet.com.cn>
2017-11-21 19:03:09 +0800

    11

---
+++ ENC/application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ENC 首页
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+}
