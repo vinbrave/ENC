dea31aeb31450c1ad928f6d19f2797a0121a437f
SC-201711132045\Administrator <aidreams@sina.com>
2017-12-24 15:10:05 +0800

    sweden 修改

--- application/views/sweden/index.php
+++ application/views/sweden/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,10 +10,14 @@
 
     <!-- load stylesheets -->
     <!--<link rel="stylesheet" href="http://fonts.useso.com/css?family=Open+Sans:300,400">  &lt;!&ndash; Google web font "Open Sans" &ndash;&gt;-->
-    <link rel="stylesheet" href="public/sweden/font-awesome-4.5.0/css/font-awesome.min.css">                <!-- Font Awesome -->
-    <link rel="stylesheet" href="public/sweden/css/bootstrap.min.css">                                      <!-- Bootstrap style -->
-    <link rel="stylesheet" href="public/sweden/css/hero-slider-style.css">                                  <!-- Hero slider style -->
-    <link rel="stylesheet" href="public/sweden/css/templatemo-style.css">                                   <!-- Templatemo style -->
+    <link rel="stylesheet" href="public/sweden/font-awesome-4.5.0/css/font-awesome.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="public/sweden/css/bootstrap.min.css">
+    <!-- Bootstrap style -->
+    <link rel="stylesheet" href="public/sweden/css/hero-slider-style.css">
+    <!-- Hero slider style -->
+    <link rel="stylesheet" href="public/sweden/css/templatemo-style.css">
+    <!-- Templatemo style -->
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -59,14 +63,14 @@
     <div class="container-fluid tm-section-3-inner">
         <div class="row">
             <div class=tm-plan-boxes-container">
-                <p class="tm-section-3-description">最具增⻓潜⼒的投资⽬的地；
-                    最⾼端的投资年度盛会；
-                    政府官员⾯对⾯指点迷津；
-                    企业家联盟机构资深⻅解；
-                    VIP专享闭⻔讨论机会；⾛进瑞典前两⼤城市和⾦融与汽⻋产业中⼼；
-                    考察医疗、AI、⼤数据、资本、新能源、智慧城市、家族企业传承、新技术等领域最具增⻓
-                    潜⼒的投资机遇；
-                    新年伊始体验北欧美⻝、美景、独特⽂化和本地⽣活。</p>
+                <p class="tm-section-3-description" style="color: #006AA7">
+                    <b>最具增长潜力的投资目的地；<br/>
+                    最高端的投资年度盛会；<br/>
+                    政府官员面对面指点迷津；<br/>
+                    企业家联盟机构资深见解；<br/>
+                    VIP专享闭门讨论机会；走进瑞典前两大城市和金融与汽车产业中心；<br/>
+                    考察医疗、AI、大数据、资本、新能源、智慧城市、家族企业传承、新技术等领域最具增长潜力的投资机遇；<br/>
+                    新年伊始体验北欧美食、美景、独特文化和本地生活。</b></p>
             </div>
         </div>
         <div class="row">
@@ -75,25 +79,20 @@
                 <h2 class="tm-section-3-title">瑞典简介</h2>
 
                 <div class="">
-                    1.瑞典简介
-                    瑞典，斯堪的纳维亚经济强国和⼯业⼤国，也是全球创新的港湾，970万维京⼈后裔
-                    的家，既历史悠久⼜引领全球创新，崇尚知识和批判性思维，孕育了汽⻋领域的沃尔
-                    沃（VOLVO）、家居领域的宜家（IKEA）、时尚快消品牌H&M，也是全世界最著名
-                    的科学和⼈⽂桂冠诺⻉尔奖（The Nobel Prize）的故乡。
-                    瑞典是创新和研发强国。在世界知识产权组织（WIPO）公布的“2017年度全球创新
-                    指数”中，瑞典创新能⼒排名全世界第⼆。早在2012年瑞典就已将国内⽣产总值
-                    （GDP）的3.4%投⼊到研发（R&D）当中，⽐例之⾼排在世界前列。
-                    瑞典是数⽬众多的跨国公司的摇篮，得益于利益关联者（stakeholders）的强烈⽂化
-                    基因，瑞典成为契约精神、合作共赢、锐意进取的典范。福布斯在2017年将瑞典列
-                    为“最适合做⽣意的国家”第⼀位。欧盟发布的“欧洲创新计分牌”将瑞典列为欧盟28个
-                    成员国第⼀位。⽽在全球声誉研究所（Reputation Institute）2016年的国家排名
-                    中，瑞典位居全球声誉榜第⼀位。
-                    瑞典⽂化包容，英语普及率居全球⾮英语国家第⼆位。瑞典⼈热爱阳光，亲近⼤⾃然，
-                    关注环境问题，注重⽣活品质，是全球最宜居的国家之⼀。
-                    2018新年伊始，跟我们⼀起去瑞典探索新的财富机会，与瑞典政、商、学界精英⾯
-                    对⾯，同欧洲最有财富的百年家族共话，探讨创新与企业家精神如何带来繁荣，体验
-                    瑞典本地化⽂化和⽣活⽅式。
 
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        瑞典，斯堪的纳维亚经济强国和工业大国，也是全球创新的港湾，970万维京人后裔的家，既历史悠久又引领全球创新，崇尚知识和批判性思维，孕育了汽车领域的沃尔沃（VOLVO）、家居领域的宜家（IKEA）、时尚快消品牌H&M，也是全世界最著名的科学和人文桂冠诺贝尔奖（The
+                        Nobel Prize）的故乡。</p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        瑞典是创新和研发强国。在世界知识产权组织（WIPO）公布的“2017年度全球创新指数”中，瑞典创新能力排名全世界第二。早在2012年瑞典就已将国内生产总值（GDP）的3.4%投入到研发（R&D）当中，比例之高排在世界前列。</p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        瑞典是数目众多的跨国公司的摇篮，得益于利益关联者（stakeholders）的强烈文化基因，瑞典成为契约精神、合作共赢、锐意进取的典范。福布斯在2017年将瑞典列为“最适合做生意的国家”第一位。欧盟发布的“欧洲创新计分牌”将瑞典列为欧盟28个成员国第一位。而在全球声誉研究所（Reputation
+                        Institute）2016年的国家排名中，瑞典位居全球声誉榜第一位。</p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        瑞典文化包容，英语普及率居全球非英语国家第二位。瑞典人热爱阳光，亲近大自然，关注环境问题，注重生活品质，是全球最宜居的国家之一。</p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        2018新年伊始，跟我们一起去瑞典探索新的财富机会，与瑞典政、商、学界精英面对面，同欧洲最有财富的百年家族共话，探讨创新与企业家精神如何带来繁荣，体验瑞典本地化文化和生活方式。
+                    </p>
                 </div>
             </div> <!-- tm-plan-boxes-container -->
 
@@ -105,18 +104,18 @@
     <div class="container-fluid tm-section-3-inner">
         <!--<div class="container-fluid tm-section tm-section-2">-->
         <div class="tm-plan-boxes-container">
-            <h2 class="tm-section-3-title">随⾏专家 </h2>
+            <h2 class="tm-section-3-title">随行专家 </h2>
         </div>
         <div class="row tm-media-row">
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <img src="public/sweden/img/Jack.jpg" alt="Image" class="img-fluid img-circle img-thumbnail tm-media-img">
+                <img src="public/sweden/img/Jack.jpg" alt="Image"
+                     class="img-fluid img-circle img-thumbnail tm-media-img">
             </div>
             <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
                 <div class="tm-media-text-container">
                     <h3 class="tm-media-title tm-gray-text">Jack</h3>
-                    <p class="tm-media-description tm-gray-text-2">⻢俊杰先⽣是昂科国际(ENC International)创始⼈、CEO。他是美
-                        国国务院国际访问者领导⼒项⽬（IVLP）访问学者，经济学者，跨
-                        ⽂化交流专家，专栏作家和中⻄⽅⽂化使者。</p>
+                    <p class="tm-media-description tm-gray-text-2">马俊杰先生是昂科国际(ENC
+                        International)创始人、CEO。他是美国国务院国际访问者领导力项目（IVLP）访问学者，经济学者，跨文化交流专家，专栏作家和中西方文化使者。</p>
                 </div>
             </div>
         </div>
@@ -124,17 +123,15 @@
         <div class="row tm-media-row tm-flex-container-reverse">
 
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 tm-flex-child-2">
-                <img src="public/sweden/img/Christer.jpg" alt="Image" class="img-fluid img-circle img-thumbnail tm-media-img">
+                <img src="public/sweden/img/Christer.jpg" alt="Image"
+                     class="img-fluid img-circle img-thumbnail tm-media-img">
             </div>
 
             <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 tm-flex-child-1">
                 <div class="tm-media-text-container-left">
                     <h3 class="tm-media-title tm-gray-text">Christer</h3>
-                    <p class="tm-media-description tm-gray-text-2">哲瑞思博⼠是昂科国际(ENC International)副总裁和研究⽹络主
-                        席。他是著名经济学家，前瑞典驻华参赞、科技创新处主管，北
-                        京⼤学汇丰商学院教授，哥本哈根商学院客座教授，与阿⾥巴巴
-                        的⻢云同列“世界互联⽹⼤会·乌镇峰会”世纪互联⽹领先科技成果
-                        推荐委员会委员。在中国⽣活15年，是中欧事务专家。</p>
+                    <p class="tm-media-description tm-gray-text-2">哲瑞思博士是昂科国际(ENC
+                        International)副总裁和研究网络主席。他是著名经济学家，前瑞典驻华参赞、科技创新处主管，北京大学汇丰商学院教授，哥本哈根商学院客座教授，与阿里巴巴的马云同列“世界互联网大会·乌镇峰会”世纪互联网领先科技成果推荐委员会委员。在中国生活15年，是中欧事务专家。</p>
                 </div>
             </div>
 
@@ -142,14 +139,14 @@
 
         <div class="row tm-media-row">
             <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <img src="public/sweden/img/Daniel.jpg" alt="Image" class="img-fluid img-circle img-thumbnail tm-media-img">
+                <img src="public/sweden/img/Daniel.jpg" alt="Image"
+                     class="img-fluid img-circle img-thumbnail tm-media-img">
             </div>
             <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
                 <div class="tm-media-text-container">
                     <h3 class="tm-media-title tm-gray-text">Daniel</h3>
-                    <p class="tm-media-description tm-gray-text-2">Daniel Ekstrom是瑞典时尚品牌TOTEM COLLECTIVE联合
-                        创始⼈，具有丰富的全球价值链和创新创业领域经验。是中
-                        瑞经贸关系的年⻘⼀代推动者。</p>
+                    <p class="tm-media-description tm-gray-text-2">Daniel Ekstrom是瑞典时尚品牌TOTEM
+                        COLLECTIVE联合创始人，具有丰富的全球价值链和创新创业领域经验。是中瑞经贸关系的年青一代推动者。</p>
                 </div>
             </div>
         </div>
@@ -158,53 +155,42 @@
 
 <section class="tm-section tm-section-3 tm-bg-light-blue">
     <div class="container-fluid tm-section-3-inner">
-        <div class="row center-block text-xs-center tm-section-3-header">
-            <div class="col-xs-12">
-                <h2 class="tm-text-white tm-section-3-title tm-text-yellow">⾏程安排</h2>
-            </div>
-        </div>
+        <!--        <div class="row center-block text-xs-center tm-section-3-header">-->
         <div class="row">
-
             <div class="tm-plan-boxes-container">
-                <!--<h2 class="tm-section-3-title tm-text-yellow">行程安排</h2>-->
+                <h2 class="tm-section-3-title tm-text-yellow">行程安排</h2>
                 <div class="">
-                    <pre>
-                        <p class="tm-text-yellow">
-                    第⼀天（2018年1⽉21⽇，周⽇）
-                    北京启程，当⽇到达瑞典第⼆⼤城市，汽⻋⼯业基地哥德堡（Gothenburg），⼊住酒店。
-                    第⼆天（2018年1⽉22⽇，周⼀）
-                    上午，访问欧盟区域商业联盟之⼀，哥德堡市政府下辖商业促进机构Business Region
-                    Gothenburg总部，了解瑞典经济发动机的动态与发展前景；
-                    下午，参加中国驻瑞典总领事馆招待会，全⾯把握中瑞贸易往来，中国对瑞典投资现状与
-                    机会；
-                    第三天（2018年1⽉23⽇，周⼆）
-                    上午受邀参加北欧最⼤的投资集团之⼀East Capital集团“Emerging Markets Conference”
-                    投资年度峰会，参会嘉宾有欧洲国家前⾸相、欧盟政府性投资机构⾼管、全球知名投资机
-                    构CEO、CFA协会主席等等。
-                    下午，参加与East Capital集团核⼼合伙⼈的“VIP财富机会闭⻔会”；下午，访问瑞典政府
-                    增⻓政策分析处（Swedish Agency for Growth Policy Analysis），并参加闭⻔研讨会；
-                    晚上，有神秘嘉宾出席的晚餐。
-                    第四天（2018年1⽉24⽇，周三）
-                    上午，访问汤森路透集团（Thomson Reuters）, 参加有关北欧房地产市场、⾼端健康、
-                    医疗科技、⼈⼯智能、⾦融科技贸易平台等⽅向的投资机会路演；
-                    有神秘嘉宾参加的午餐会；
-                    下午，访问北欧最⼤也是最知名的投资银⾏集团瑞典北欧斯安银⾏（SEB），并与瓦伦堡
-                    （Wallenberg）家族董事会成员闭⻔研讨瑞典财富机会。
-                    考察团亲密晚餐。
-                    第五天（2018年1⽉25⽇，周四）
-                    上午，访问瑞典企业家论坛（Swedish Entrepreneurship Forum），并参加“企业家精神
-                    与创新”专题圆桌座谈会。参会嘉宾有瑞典皇家理⼯⼤学教授、瑞典政府官员等；
-                    与瑞典企业家论坛专家⽤午餐；
-                    下午，访问瑞典ILV环境研究所（IVL Swedish Environmental Research Institute），了解
-                    智慧城市平台和投资机会；
-                    ⾃由⽤晚餐。
-                    第六天（2018年1⽉26⽇，周五）
-                    当地⽂化体验⽇
-                    参观Vassa博物馆，感受瑞典本地化⽣活，体验Fika⽂化，⾃由活动, 打折季购物狂欢。
-                    第七天（2018年1⽉27⽇，周六）
-                    从斯德哥尔摩启程返回北京。
-                        </p>
-                        </pre>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">
+                        第一天（2018年1月21日，周日）北京启程，当日到达瑞典第二大城市，汽车工业基地哥德堡（Gothenburg），入住酒店。
+                    </p>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">第二天（2018年1月22日，周一）
+                        上午，访问欧盟区域商业联盟之一，哥德堡市政府下辖商业促进机构Business Region Gothenburg总部，了解瑞典经济发动机的动态与发展前景；
+                        下午，参加中国驻瑞典总领事馆招待会，全面把握中瑞贸易往来，中国对瑞典投资现状与机会；
+                    </p>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">第三天（2018年1月23日，周二）
+                        上午受邀参加北欧最大的投资集团之一East Capital集团“Emerging Markets
+                        Conference”投资年度峰会，参会嘉宾有欧洲国家前首相、欧盟政府性投资机构高管、全球知名投资机构CEO、CFA协会主席等等。
+                        下午，参加与East Capital集团核心合伙人的“VIP财富机会闭门会”；下午，访问瑞典政府增长政策分析处（Swedish Agency for Growth Policy
+                        Analysis），并参加闭门研讨会；
+                        晚上，有神秘嘉宾出席的晚餐。
+                    </p>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">第四天（2018年1月24日，周三）
+                        上午，访问汤森路透集团（Thomson Reuters）, 参加有关北欧房地产市场、高端健康、医疗科技、人工智能、金融科技贸易平台等方向的投资机会路演；
+                        有神秘嘉宾参加的午餐会；
+                        下午，访问北欧最大也是最知名的银行集团瓦伦堡 （Wallenberg）家族所有的瑞典北欧斯安银行（SEB），并与 董事会成员闭门研讨瑞典财富机会。
+                        考察团亲密晚餐。
+                    </p>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">第五天（2018年1月25日，周四）
+                        上午，访问瑞典企业家论坛（Swedish Entrepreneurship Forum），并参加“企业家精神与创新”专题圆桌座谈会。参会嘉宾有瑞典皇家理工大学教授、瑞典政府官员等；
+                        与瑞典企业家论坛专家用午餐；
+                        下午，访问瑞典ILV环境研究所（IVL Swedish Environmental Research Institute），了解智慧城市平台和投资机会；
+                        自由用晚餐。</p>
+                    <p class="tm-section-3-description tm-text-yellow" style="line-height:1.5;">第六天（2018年1月26日，周五）
+                        当地文化体验日
+                        参观Vassa博物馆，感受瑞典本地化生活，体验Fika文化，自由活动, 打折季购物狂欢。
+                        第七天（2018年1月27日，周六）
+                        从斯德哥尔摩启程返回北京。
+                    </p>
                 </div>
             </div> <!-- tm-plan-boxes-container -->
 
@@ -220,32 +206,37 @@
                 <h2 class="tm-section-3-title">参团信息</h2>
 
                 <div class="">
-                    <pre>
-                    此次“财富·瑞典—主题投资考察”得到瑞典驻华⼤使馆、东⽅资本集团（East
-                    Capital），中国驻瑞典总领事馆、瑞典增⻓政策分析处、瑞典企业家论坛、瑞典
-                    北欧斯安银⾏（SEB）、瑞典ILV环境研究所（IVL Swedish Environmental
-                    Research Institute）等机构的⼤⼒⽀持和全⼒配合。
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        此次“财富·瑞典—主题投资考察”得到瑞典驻华⼤使馆、东⽅资本集团（East
+                        Capital），中国驻瑞典总领事馆、瑞典增⻓政策分析处、瑞典企业家论坛、瑞典
+                        北欧斯安银⾏（SEB）、瑞典ILV环境研究所（IVL Swedish Environmental
+                        Research Institute）等机构的⼤⼒⽀持和全⼒配合。
+                    </p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">为保证考察活动的内容和质量，仅设<span
+                                style="font-size: 30px;color: #b2b300"><u><b>10</b></u></span>个VIP席位，您的同⾏者将是和您⼀样
+                        有着独到眼光和投资哲学的精英⼈⼠。</p>
 
-                    为保证考察活动的内容和质量，仅设<span style="font-size: 30px;color: #b2b300"><u><b>10</b></u></span>个VIP席位，您的同⾏者将是和您⼀样
-                    有着独到眼光和投资哲学的精英⼈⼠。
+                    <p class="tm-section-3-description" style="line-height:1.5;">参团费⽤“⼀揽⼦计划”为：<span
+                                style="font-size: 35px;color: #ff0000"><u><b>52,600</b></u></span>元/⼈
+                        费⽤包括：中国-瑞典往返经济舱机票，四星级酒店住宿，保险，⽬的地交通(不
+                        含斯德哥尔摩和哥德堡之间的⽕⻋票)。</p>
 
-                    参团费⽤“⼀揽⼦计划”为：<span style="font-size: 35px;color: #ff0000"><u><b>52,600</b></u></span>元/⼈
-                    费⽤包括：中国-瑞典往返经济舱机票，四星级酒店住宿，保险，⽬的地交通(不
-                    含斯德哥尔摩和哥德堡之间的⽕⻋票)。
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        如⾃⾏解决机票和酒店，则参团费⽤为：45,900元/⼈。</p>
 
-                    如⾃⾏解决机票和酒店，则参团费⽤为：45,900元/⼈。
-
-                    考察团成员还可得到办理赴瑞典签证的加急协助（费⽤⾃理）。
+                    <p class="tm-section-3-description" style="line-height:1.5;">
+                        考察团成员还可得到办理赴瑞典签证的加急协助（费⽤⾃理）。
 
                         <span style="color: #ff0000">前五位报名缴纳定⾦者（定⾦12,000元，特别提醒：如取消报名，定⾦不退）
                         可享受九折优惠（即参团费为47,300元/⼈）。</span>
+                    </p>
+                    <p class="tm-section-3-description" style="line-height:1.5;">报名截⽌⽇期：2018年1⽉11⽇<br />
 
-                    报名截⽌⽇期：2018年1⽉11⽇
-                    咨询电话：186 0070 8808
-                    汇款信息：开户⾏：中国农业银⾏天津北⾠⽀⾏营业部
-                             账户名：昂科（天津）⽂化传播有限公司
-                             账 号：02040001040066540
-                        </pre>
+                        咨询电话：186 0070 8808<br/>
+                        汇款信息：开户⾏：中国农业银⾏天津北⾠⽀⾏营业部<br />
+                                 <span style="margin-left: 95px"> 账户名：昂科（天津）⽂化传播有限公司</span><br />
+                                <span style="margin-left: 95px"> 账 号：02040001040066540</span><br />
+                    </p>
                 </div>
             </div> <!-- tm-plan-boxes-container -->
 
@@ -258,7 +249,7 @@
         <div class="row">
             <div class="tm-plan-boxes-container">
                 <h2 class="tm-section-3-title">线下社群</h2>
-                <div class="row row center-block">
+                <div class="row center-block">
                     <p class="tm-section-3-description">昂科企业家共创⽹络（ENC Entrepreneur Co-Creation Network）是⼀个集学
                         者、企业家、政治精英为⼀体，致⼒于促进经济政策研究分析、政治经济形势判
                         断、企业家群体智识提升和财富发现的⾼端精英俱乐部。
@@ -274,9 +265,12 @@
 <script src="public/sweden/js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
 <script src="public/sweden/js/tether.min.js"></script>
 <!-- Tether for Bootstrap (http://stackoverflow.com/questions/34567939/how-to-fix-the-error-error-bootstrap-tooltips-require-tether-http-github-h) -->
-<script src="public/sweden/js/bootstrap.min.js"></script>                 <!-- Bootstrap js (v4-alpha.getbootstrap.com/) -->
-<script src="public/sweden/js/hero-slider-script.js"></script>            <!-- Hero slider (https://codyhouse.co/gem/hero-slider/) -->
-<script src="public/sweden/js/jquery.touchSwipe.min.js"></script>         <!-- http://labs.rampinteractive.co.uk/touchSwipe/demos/ -->
+<script src="public/sweden/js/bootstrap.min.js"></script>
+<!-- Bootstrap js (v4-alpha.getbootstrap.com/) -->
+<script src="public/sweden/js/hero-slider-script.js"></script>
+<!-- Hero slider (https://codyhouse.co/gem/hero-slider/) -->
+<script src="public/sweden/js/jquery.touchSwipe.min.js"></script>
+<!-- http://labs.rampinteractive.co.uk/touchSwipe/demos/ -->
 <script>
 
     $(document).ready(function () {
